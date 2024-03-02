680a2dc9e0ba2a3a782b8514821384e51f4d6664
shivraj0002 <shivrajdange2710@gmail.com>
2024-03-02 17:40:01 +0530

    Completed the project

--- src/index.js
+++ src/index.js
@@ -2,13 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
+//  use React Bootstrap for better styling 
+import "bootstrap/dist/css/bootstrap.min.css";
+// import 'react-toastify/dist/ReactToastify.css';
+import 'react-toastify/dist/ReactToastify.css'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <BrowserRouter>
   <React.StrictMode>
     <App />
   </React.StrictMode>
+</BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
